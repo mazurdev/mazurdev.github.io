42dc899c92dcbeb0b42169f52c6a11b296bbe8b1
vladisrav <vlad.developm@gmail.com>
2019-01-14 23:14:56 +0200

    add random-names

---
+++ random-names/README.md
@@ -0,0 +1,30 @@
+<p align="center">
+  <a href="https://github.com/mazurdev/random-names">
+    <img src="https://raw.githubusercontent.com/mazurdev/random-names/master/meta-assets/icon.png" alt="Icon random" width="96" height="96">
+  </a>
+  <h1 align="center">RANDOM NAMES</h1>
+  <p align="center">Mini service for generating random names</p>
+  <p align="center">
+   <a href="https://twitter.com/mazurdev"><img src="https://img.shields.io/badge/feedback-@mazurdev-blue.svg" /></a>
+  </p>
+  <br>
+</p>
+
+<sub>Check out my [blog-portfolio](https://mazurdev.com/)</sub>
+
+## INTRO
+
+![intro](meta-assets/intro.png)
+
+## :wrench: STACK
+
+* JavaScript
+* Normalize
+* Skeleton
+
+## :heavy_check_mark: GETTING STARTED
+```bash
+git clone https://github.com/mazurdev/random-names.git
+cd to folder
+open "index.html" in your browser
+```
---
+++ random-names/js/main.js
@@ -0,0 +1,56 @@
+document.querySelector('#random-names').addEventListener('submit', loadNames);
+
+// Execute the function to query the API
+function loadNames(e) {
+	e.preventDefault();
+
+
+	// Read the values from the form and create the variables
+	const origin = document.getElementById('country').value;
+	const genre = document.getElementById('genre').value;
+	const amount = document.getElementById('quantity').value;
+
+	// Build the URL
+	let url = 'http://uinames.com/api/?';
+	// Read the origin and append to the url
+	if (origin !== '') {
+		url += `region=${origin}&`;
+	}
+	// Read the genre and append to the url
+	if (genre !== '') {
+		url += `gender=${genre}&`;
+	}
+	// Read the amount and append to the url
+	if (amount !== '') {
+		url += `amount=${amount}&`;
+	}
+
+	// Ajax Call
+	const xhr = new XMLHttpRequest();
+
+	// Open the connection
+	xhr.open('GET', url, true);
+
+	// Execute the function
+	xhr.onload = function () {
+		if (this.status === 200) {
+			const names = JSON.parse(this.responseText);
+
+			// Insert into the HTML
+
+			let html = '<h2 class="generate-title">Names Received</h2>';
+			html += '<ol class="generate-list card-shadow">';
+			names.forEach(function (name) {
+				html += `
+                         <li>${name.name}</li>
+                    `;
+			});
+			html += '</ol>';
+
+			document.querySelector('#result').innerHTML = html;
+		}
+	}
+
+	// Send the request
+	xhr.send();
+}
